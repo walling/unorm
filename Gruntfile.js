c72c18f4ced3a3289ae68245331359474e260720
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-02-12 22:04:57 +0200

    Add quotmark: double check

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,54 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		jshint: {
 			options: {
-				jshintrc: '.jshintrc'
+				jshintrc: ".jshintrc"
 			},
 			gruntfile: {
-				src: 'Gruntfile.js',
+				src: "Gruntfile.js",
 			},
 			lib: {
-				src: ['lib/**/*.js']
+				src: ["lib/**/*.js"]
 			},
 			tests: {
-				src: ['test/**/*.js'],
+				src: ["test/**/*.js"],
 			},
 			examples: {
-				src: ['examples/**/*.js']
+				src: ["examples/**/*.js"]
 			},
 		},
 		watch: {
 			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
+				files: "<%= jshint.gruntfile.src %>",
+				tasks: ["jshint:gruntfile"]
 			},
 			lib: {
-				files: '<%= jshint.lib.src %>',
-				tasks: ['jshint:lib']
+				files: "<%= jshint.lib.src %>",
+				tasks: ["jshint:lib"]
 			},
 		},
 		simplemocha: {
 			options: {
-				globals: ['should'],
+				globals: ["should"],
 				timeout: 3000,
-				ui: 'bdd',
-				reporter: 'spec'
+				ui: "bdd",
+				reporter: "spec"
 			},
 
-			all: { src: 'test/**/*.js' }
+			all: { src: "test/**/*.js" }
 		}
 	});
 
 	// Tasks
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-simple-mocha');
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-simple-mocha");
 
 	// Default task.
-	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('test', ['jshint', 'simplemocha']);
+	grunt.registerTask("default", ["jshint"]);
+	grunt.registerTask("test", ["jshint", "simplemocha"]);
 };
--- examples/basic-usage/basic-usage-in-node.js
+++ examples/basic-usage/basic-usage-in-node.js
@@ -1,44 +1,44 @@
 /*jshint node:true*/
-'use strict';
+"use strict";
 
-require('unorm');
+require("unorm");
 
 // Function to display Unicode codepoints of a string.
 function codepoints(string) {
-	return string.split('').map(function(chr) {
+	return string.split("").map(function(chr) {
 		var codepoint = chr.charCodeAt(0);
 		return (codepoint >= 33 && codepoint <= 126) ?
 			JSON.stringify(chr) :
-			'U+' + codepoint.toString(16).toUpperCase();
-	}).join(' ');
+			"U+" + codepoint.toString(16).toUpperCase();
+	}).join(" ");
 }
 
 // Scientific Ångström symbol is converted to Scandinavian letter Å.
-var angstrom = '\u212B';
-console.log('- Example 1 -');
+var angstrom = "\u212B";
+console.log("- Example 1 -");
 console.log(codepoints(angstrom));
-console.log(codepoints(angstrom.normalize('NFC')));
+console.log(codepoints(angstrom.normalize("NFC")));
 
 // German ä and ü decomposed into a and u with Combining Diaeresis character.
-var letters = '\u00E4\u00FC';
-console.log('- Example 2 -');
+var letters = "\u00E4\u00FC";
+console.log("- Example 2 -");
 console.log(codepoints(letters));
-console.log(codepoints(letters.normalize('NFD')));
+console.log(codepoints(letters.normalize("NFD")));
 
 // String optimized for compatibility, ie. CO₂ becomes CO2.
-var scientific = 'CO\u2082 and E=mc\u00B2';
-console.log('- Example 3 -');
+var scientific = "CO\u2082 and E=mc\u00B2";
+console.log("- Example 3 -");
 console.log(scientific);
-console.log(scientific.normalize('NFKC'));
+console.log(scientific.normalize("NFKC"));
 
 // NOTE: Rest of the example requires XRegExp: npm install xregexp
 
 // Remove combining characters / marks from Swedish name, ie. ö becomes o.
 // This is useful for indexing and searching internationalized text.
 try {
-	var xregexp = require('xregexp').XRegExp;
-	var name = '\u00C5ngstr\u00F6m';
-	console.log('- Example 4 -');
-	console.log(name.normalize('NFKD'));
-	console.log(name.normalize('NFKD').replace(xregexp('\\p{M}', 'g'), ''));
+	var xregexp = require("xregexp").XRegExp;
+	var name = "\u00C5ngstr\u00F6m";
+	console.log("- Example 4 -");
+	console.log(name.normalize("NFKD"));
+	console.log(name.normalize("NFKD").replace(xregexp("\\p{M}", "g"), ""));
 } catch (ex) {}
